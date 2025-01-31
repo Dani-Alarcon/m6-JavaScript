6fec625b2b6888e205c7b6b5e35415608f4f44fc
Dani <daniel.alarcon.7e6@itb.cat>
2025-01-31 12:07:28 +0000

    ajedrez divididio en funciones

--- Ejemplos/Ejemplo11:Ajedrez/ajedrez.js
+++ Ejemplos/Ejemplo11:Ajedrez/ajedrez.js
@@ -1,40 +1,49 @@
 //Ajedrez solo con un for
-let tablero = document.getElementById("tablero");
+let tablero = document.getElementById("tablero")
 
 for (let i = 0; i < 64; i++) {
-    let num = document.createTextNode("");
-    let cuadrado = document.createElement('p');
-    cuadrado.className = "casilla";
+    let num = document.createTextNode("")
+    let cuadrado = document.createElement('p')
+    cuadrado.className = "casilla"
 
-    estilosTablero(i, cuadrado, num);
+    estilosTablero(i, cuadrado)
+    fichas(i, num)
 
-    tablero.appendChild(cuadrado);
-    cuadrado.appendChild(num);
+    tablero.appendChild(cuadrado)
+    cuadrado.appendChild(num)
 }
 
-function estilosTablero(i, cuadrado, num) {
-   
+function estilosTablero(i, cuadrado) {
     if (parseInt(i / 8) % 2 == 0) {
         if (i % 2 == 0) {
-            cuadrado.style.backgroundColor = 'black';
-            cuadrado.style.color = 'white';
+            fondoNegro(cuadrado)
         } else {
-            cuadrado.style.backgroundColor = 'white';
-            cuadrado.style.color = 'black';
+            fondoBlanco(cuadrado)
         }
     } else {
         if (i % 2 == 0) {
-            cuadrado.style.backgroundColor = 'white';
-            cuadrado.style.color = 'black';
+            fondoBlanco(cuadrado)
         } else {
-            cuadrado.style.backgroundColor = 'black';
-            cuadrado.style.color = 'white';
+            fondoNegro(cuadrado)
         }
     }
+}
 
+function fichas(i, num) {
     if (i < 16) {
-        num.textContent = "O";  
+        num.textContent = "O"
     } else if (i >= 48) {
-        num.textContent = "X";  
+        num.textContent = "X"
     }
+}
+
+function fondoNegro(cuadrado) {
+    cuadrado.style.backgroundColor = 'black'
+    cuadrado.style.color = 'white'
+
+}
+
+function fondoBlanco(cuadrado) {
+    cuadrado.style.backgroundColor = 'white'
+    cuadrado.style.color = 'black'
 }
