73b19a9eb7cd8ab5a431cd67c98657cf434db012
Dani <daniel.alarcon.7e6@itb.cat>
2025-03-07 12:12:34 +0000

    correccion tarjeta

--- Ejemplos/tarjetaCredito/TarjetaCredito.js
+++ Ejemplos/tarjetaCredito/TarjetaCredito.js
@@ -24,7 +24,7 @@ class TarjetaCredito {
         return this.dinero
     }
     pagar(precio) {
-        if (this.dinero > precio) {
+        if (this.dinero >= precio) {
             this.dinero -= precio
         } else {
             alert('No puedes pagar esto')
@@ -45,8 +45,10 @@ let tarjeta3 = new TarjetaCredito(454894564, 789, '05/26', 789)
 console.log('Activar tarjeta ' + tarjeta1.activar())
 
 let dinero = prompt('Cuanto dinero quieres ingresar en la tarjeta?')
-console.log('Tu saldo despues de ingresar es:' + tarjeta1.ingresarDinero(dinero))
+console.log('Tu saldo despues de ingresar es:' + tarjeta1.ingresarDinero(dinero) + '€')
 
-console.log('Tu saldo despues de pagar es: ' + tarjeta1.pagar(10))
+console.log('Tu saldo despues de pagar es: ' + tarjeta1.pagar(10) + '€')
 
 console.log('Ahora tu pin es: ' + tarjeta1.cambiarPin())
+
+console.log(tarjeta1)
