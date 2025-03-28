6410fd900f43173489ee929e13969254b3515f91
Dani <daniel.alarcon.7e6@itb.cat>
2025-03-28 11:04:14 +0000

    quitar puntos

--- Ejemplos/cookies/cookiesVentana.js
+++ Ejemplos/cookies/cookiesVentana.js
@@ -1,23 +1,23 @@
 // Función para guardar las cookies con los colores seleccionados
 function crearCookies() {
-    let fondo = document.getElementById('fondo').value;
-    let fuente = document.getElementById('fuente').value;
+    let fondo = document.getElementById('fondo').value
+    let fuente = document.getElementById('fuente').value
 
-    document.cookie = `fondo=${fondo}; path=/; max-age=31536000`;
-    document.cookie = `fuente=${fuente}; path=/; max-age=31536000`;
-    document.cookie = `configurada=true; path=/; max-age=31536000`;
+    document.cookie = `fondo=${fondo}; `
+    document.cookie = `fuente=${fuente}; `
+    document.cookie = `configurada=true;`
 
     // Recarga la página principal y cierra la ventana
     if (window.opener) {
-        window.opener.location.reload();
+        window.opener.location.reload()
     }
-    window.close();
+    window.close()
 }
 
 // Código que se ejecuta cuando la ventana emergente ha cargado
 document.addEventListener("DOMContentLoaded", () => {
-    let botonGuardar = document.getElementById('guardar');
+    let botonGuardar = document.getElementById('guardar')
     if (botonGuardar) {
-        botonGuardar.addEventListener('click', crearCookies);
+        botonGuardar.addEventListener('click', crearCookies)
     }
 });
