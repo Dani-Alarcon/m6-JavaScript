627eec780b5b58db97b0bca659d56804331b593d
Dani <daniel.alarcon.7e6@itb.cat>
2025-04-25 11:19:12 +0000

    pokemon await va bien

--- Ejemplos/API/pokeApi.js
+++ Ejemplos/API/pokeApi.js
@@ -1,9 +1,18 @@
 function init() {
     document.getElementById("masChistes").addEventListener("click",conectaAPI);
     }
-    function conectaAPI(){
+    async function conectaAPI(){
         let id = Math.floor(Math.random() * 1025)
-        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        try {
+            let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`); // Esperar la respuesta
+            let data = await response.json();
+            console.log("Pokémon obtenido:", data);
+            muestraPokemon(data.name, data.sprites.front_shiny);
+        } catch (error) {
+            console.error("Error al obtener datos", error);
+        }
+     
+        /*fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
            .then(res => {
                if(res.ok)
                    // respuesta ok, ahora queremos el json con los datos.
@@ -13,7 +22,7 @@ function init() {
                console.log(json);
                //Tratamos la información recibida
                muestraPokemon(json.name, json.sprites.front_shiny);
-           });
+           });*/
     }
     function muestraPokemon(nombre, imagenURL) {
         console.log(nombre);
